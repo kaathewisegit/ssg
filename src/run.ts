51ca823856e674cf231e328fc643d463f0ded9c7
Andrej Kolčin <self@kaathewise.net>
2025-09-12 15:25:36 +0300

    [src] run: create a CLI entrypoint

---
+++ src/run.ts
@@ -0,0 +1,45 @@
+#!/usr/bin/env bun
+
+import * as fs from "node:fs/promises"
+import * as path from "node:path"
+import { build, type Config, serve } from "./index"
+
+const CONFIG_PATH = "ssg.config.ts"
+const HELP = `Usage: ssg <command>
+
+Commands:
+    build:              build the website
+    dev:                start a hot-reloading development server
+`
+
+const args = process.argv.slice(2)
+
+const configPath = path.resolve(CONFIG_PATH)
+if (!(await fs.exists(configPath))) {
+	console.warn("Configuration file not found")
+	process.exit(10)
+}
+
+const configModule = await import(configPath)
+const config = configModule.default as Config
+
+switch (args[0]) {
+	case "build": {
+		await build(config)
+		break
+	}
+	case "dev": {
+		await serve(config)
+		break
+	}
+	case undefined: {
+		console.log(HELP)
+		break
+	}
+	default: {
+		console.log(
+			`The command must be either 'build' or 'dev', got ${args[0]}`,
+		)
+		process.exit(11)
+	}
+}
